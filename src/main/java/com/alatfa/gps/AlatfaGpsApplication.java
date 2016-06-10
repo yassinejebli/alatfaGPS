45ae53a341be8598731fecfe837fc737e10dc05a
yassinejebli <jebli.yassine@gmail.com>
2016-06-10 18:57:30 +0200

    propertiiies

--- src/main/java/com/alatfa/gps/AlatfaGpsApplication.java
+++ src/main/java/com/alatfa/gps/AlatfaGpsApplication.java
@@ -12,13 +12,10 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 import java.io.IOException;
 
 @SpringBootApplication
-public class AlatfaGpsApplication /* implements CommandLineRunner*/{
+public class AlatfaGpsApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(AlatfaGpsApplication.class, args);
-
-
 	}
 
 
@@ -37,7 +34,7 @@ public class AlatfaGpsApplication /* implements CommandLineRunner*/{
 	@Autowired
 	private GeoZoneAlertRepository geoZoneAlertRepository;
 
-/*	@Override
+	@Override
 	public void run(String... args) throws Exception {
 
 		//System.out.println(vehiculeRepository.findByEquipementGps_imei("868683023129806"));
@@ -46,9 +43,10 @@ public class AlatfaGpsApplication /* implements CommandLineRunner*/{
 		Socket_tk103 server = null;
 		try {
 			server = new Socket_tk103(port,vehiculeRepository,positionRepository,notificationRepository,geoZoneLimitRepository,geoZoneAlertRepository);
+			server.waitForConnections();
 		} catch (IOException e) {
 			e.printStackTrace(System.err);
+			server.waitForConnections();
 		}
-		server.waitForConnections();
-	}*/
+	}
 }
